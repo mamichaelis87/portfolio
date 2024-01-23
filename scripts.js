939811fbf7f858671fb79e91f8e9652b2ade7109
Mark Michaelis <mallenm1993@gmail.com>
2024-01-22 21:02:10 -0500

    Smooth out menu button animations

--- scripts.js
+++ scripts.js
@@ -1,6 +1,13 @@
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".menu-button").addEventListener('click', function() {
-  document.querySelector('.nav-links').style.display = 
-  (document.querySelector('.nav-links').style.display == 'none') ? 'flex' : 'none';
+    if (document.querySelector('.nav-links').classList.contains("hide")) {
+      document.querySelector('.nav-links').style.display = 'flex';
+      document.querySelector('.nav-links').classList.remove("hide");
+      document.querySelector('.nav-links').classList.add("show");
+    }
+    else {
+      document.querySelector('.nav-links').classList.remove("show");
+      document.querySelector('.nav-links').classList.add("hide");
+    }
 });
 });
